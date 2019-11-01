9076ce64e7754a0a87a17c7f9d05e4dc8be93d5c
Danny Guo <dannyguo91@gmail.com>
2019-10-31 23:06:56 -0400

    Handle negative numbers, 0, and 1

--- assembly/index.ts
+++ assembly/index.ts
@@ -1,4 +1,8 @@
 export function isPrime(x: u32): bool {
+    if (x < 2) {
+        return false;
+    }
+
     for (let i: u32 = 2; i < x; i++) {
         if (x % i === 0) {
             return false;
--- benchmark.js
+++ benchmark.js
@@ -3,6 +3,10 @@ const Benchmark = require('benchmark');
 const assemblyScriptIsPrime = require('./index').isPrime;
 
 function isPrime(x) {
+    if (x < 2) {
+        return false;
+    }
+
     for (let i = 2; i < x; i++) {
         if (x % i === 0) {
             return false;
